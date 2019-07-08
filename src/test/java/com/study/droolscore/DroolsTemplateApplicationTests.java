74d78071197ac8433e8a1a4858bfb359f6ad5af4
wujiaming <wujiaming@iplas.com.cn>
2019-07-08 16:58:59 +0800

    push

--- src/test/java/com/study/droolscore/DroolsTemplateApplicationTests.java
+++ src/test/java/com/study/droolscore/DroolsTemplateApplicationTests.java
@@ -12,6 +12,14 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * //TODO
+ * 目前匹配规则不满足业务，例如 套餐是：巨无霸，可乐，薯条
+ * 但是喔购买的是：巨无霸，可乐，薯条，麦乐鸡
+ * 这种情况下无法匹配，这里需要统计一个满足条件的合集，用于给客户端获取最优解。
+ * 这里可以通过在shoppingCar中增加map属性，套餐name->totalMoney
+ * 做更细致的扩展，这里只是demo，所以暂时不考虑太复杂的情景。
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DroolsTemplateApplicationTests {
